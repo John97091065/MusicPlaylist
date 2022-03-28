c1cbebb9faeaa7d32c616466528f851105eacce0
John97091065 <97091065@st.deltion.nl>
2022-03-28 13:02:39 +0200

    Add project files.

---
+++ MusicPlaylist/code/XML/SongMethods.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Data;
+
+namespace MusicPlaylist.Code.XML
+{
+    public class SongMethods
+    {
+        public DataSet GetAllSongs(string file)
+        {
+            DataSet ds = new DataSet("playlist");
+
+            DataTable dtSongs = new DataTable("song");
+
+            DataColumn dcId = new DataColumn("id");
+            DataColumn dcArtist = new DataColumn("artist");
+            DataColumn dcTitle = new DataColumn("title");
+            DataColumn dcYear = new DataColumn("year");
+            DataColumn dcGenre = new DataColumn("genre");
+            DataColumn dcTime = new DataColumn("time");
+
+            dtSongs.Columns.Add(dcId);
+            dtSongs.Columns.Add(dcArtist);
+            dtSongs.Columns.Add(dcTitle);
+            dtSongs.Columns.Add(dcYear);
+            dtSongs.Columns.Add(dcGenre);
+            dtSongs.Columns.Add(dcTime);
+
+            ds.Tables.Add(dtSongs);
+
+            ds.ReadXml(Environment.CurrentDirectory + file);
+
+            return ds;
+        }
+    }
+}
